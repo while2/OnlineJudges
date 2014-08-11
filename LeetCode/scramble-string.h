f9c5acf386d9e96e3e9ed4ef9dfee377d04326d3
while2 <heyi_lab@126.com>
2014-08-11 19:58:22 +0800

    scamble-string AC

--- LeetCode/scramble-string.h
+++ LeetCode/scramble-string.h
@@ -9,18 +9,36 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
-
 class Solution {
 public:
     bool isScramble(string str1, string str2) {
+		if (str1.size() != str2.size())
+			return false;
+		vector<int> sum1(str1.size()+1);
+		vector<int> sum2(str1.size()+1);
+		sum1[0] = sum2[0] = 0;
+		for (int i = 0; i < str1.size(); ++i) {
+			sum1[i+1] = sum1[i] + str1[i]-'a';
+			sum2[i+1] = sum2[i] + str2[i]-'a';
+		}
+
         function<bool(int,int,int,int)> parse;
         parse = [&](int s1, int e1, int s2, int e2) {
-		
+            //if (e1 - s1 != e2 - s2) return false;
+
+			if (sum1[e1] - sum1[s1] != sum2[e2] - sum2[s2])
+				return false;
+
 			vector<int> hist(26, 0);
-			for (int i = 0; i < e1-s1; ++i) {
-				hist[str1[s1+i]-'a']++;
-				hist[str2[s2+i]-'a']--;
+			for (int i = s1; i < e1; ++i) {
+				++hist[str1[i]-'a'];
 			}
+			
+			for (int i = s2; i < e2; ++i) {
+				if (--hist[str2[i]-'a'] < 0)
+					return false;
+			}
+
 			for (auto i : hist)
 				if (i != 0)
 					return false;
@@ -39,6 +57,7 @@ public:
 			}
 			return false;
         };
+
         auto ret = parse(0, str1.size(), 0, str2.size());
 		return ret;
     }
